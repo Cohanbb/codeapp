3730e3dc6388f9ba3a01c4dec7c0bc5489a4e63f
Cohanbb <76557867+Cohanbb@users.noreply.github.com>
2022-06-06 01:00:08 +0800

    Update SeqList.cpp

--- cpp/DS/SeqList.cpp
+++ cpp/DS/SeqList.cpp
@@ -21,10 +21,10 @@ bool Empty(sqlist L) {
 void InsertElem(sqlist &L, int i, int e) {
     if (i <= 0 || i > L.length + 1) {
         printf("Invalid Position!\n");
-	    return;
+	return;
     }
     if (L.length == Maxsize) {
-	    printf("Oversize!");
+	printf("Oversize!");
         return;
     }
     for (int j = L.length; j >= i; j--)
@@ -36,7 +36,7 @@ void InsertElem(sqlist &L, int i, int e) {
 void DeleteElem(sqlist &L, int i, int &e) {
     if (i <= 0 || i > L.length) {
         printf("Invalid Position!");
-	    return;
+	return;
     }
     e = L.data[i-1];
     for (int j = i; j < L.length; j++)
@@ -47,7 +47,7 @@ void DeleteElem(sqlist &L, int i, int &e) {
 
 void PrintList(sqlist L) {
     for (int i = 1; i <= L.length; i++) 
-	    printf("%d\n", L.data[i - 1]);
+	printf("%d\n", L.data[i - 1]);
 }
 
 void LocateElem(sqlist L, int &i, int e) {
