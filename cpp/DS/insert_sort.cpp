62f44f1f8d8bb1cd56c3661fe6e9248c8c67dc2b
Cohanbb <cohanbb777@163.com>
2022-06-06 09:17:57 +0800

    update swap_sort and insert_sort

--- cpp/DS/insert_sort.cpp
+++ cpp/DS/insert_sort.cpp
@@ -2,62 +2,66 @@
 
 /*Insert Sort 直接插入排序*/
 void insert_sort(int a[], int n) {
-    //Differing from other algorithms, insert sorting deals with a[1] ~ a[n] 
-    int i, j;
-    for (i = 2; i <= n; i++)
+    int temp, i, j; //temp conserves element temporarily
+    for (i = 1; i < n; i++)
         if (a[i] < a[i - 1]) {
-            a[0] = a[i];
-            for (j = i - 1; a[0] < a[j]; j--) //find the position to insert
+            temp = a[i];
+            for (j = i - 1; j >= 0 && temp < a[j]; j--) //find the position to insert
                 a[j + 1] = a[j]; //move backwards to making room for inserting
-            a[j + 1] = a[0]; //insert the element
+            a[j + 1] = temp; //insert the element
         }
 }
 
 /*Binary Insert Sort 折半插入排序*/
 void insert_sortt(int a[], int n) {
-    int low, high, mid, i, j;
-    for (i = 2; i <= n; i++) {
-        low = 1; 
+    int low, high, mid, temp, i, j; //step is length of per comparison
+    for (i = 1; i < n; i++) {
+        low = 0;
         high = i - 1;
-        a[0] = a[i];
+        temp = a[i];
         while (low <= high) {
             mid = (low + high) / 2;
-            if (a[mid] > a[0])
+            if (a[mid] > temp)
                 high = mid - 1; 
             else
                 low = mid + 1;
         }
         for (j = i - 1; j >= high + 1; j--) 
             a[j + 1] = a[j];
-        a[high + 1] = a[0];
+        a[high + 1] = temp;
     }
 }
 
 /*Shell Sort 希尔排序*/
 void shell_sort(int a[], int n) {
-    
+    int step, temp, i, j;
+    for (step = n/2; step >= 1; step /= 2)
+        for (i = step; i < n; i++) 
+            if (a[i] < a[i - step]) {
+                temp = a[i];
+                for (j = i - step; j >= 0 && temp < a[j]; j -= step)
+                    a[j + step] = a[j];
+                a[j + step] = temp;
+            }
 }
 
 
 int main() {
     int a[11];
-    for (int i = 1; i < 11; i++)
+    for (int i = 0; i < 10; i++)
         scanf("%d", &a[i]);
     insert_sort(a, 10);
-    for (int i = 1; i < 11; i++) {
-        printf("Insert Sort: \n");
+    printf("Insert Sort: \n");
+    for (int i = 0; i < 10; i++)
         printf("%d\n", a[i]);
-    }
     insert_sortt(a, 10);
-    for (int i = 1; i < 11; i++) {
-        printf("Binary Insert Sort: \n");
+    printf("Binary Insert Sort: \n");
+    for (int i = 0; i < 10; i++)
         printf("%d\n", a[i]);
-    }
     shell_sort(a, 10);
-    for (int i = 1; i < 11; i++) {
-        printf("Shell Sort: \n");
+    printf("Shell Sort: \n");
+    for (int i = 0; i < 10; i++)
         printf("%d\n", a[i]);
-    }
     return 0;
 }
 
--- cpp/DS/swap_sort.cpp
+++ cpp/DS/swap_sort.cpp
@@ -6,9 +6,9 @@ void bubble_sort(int a[], int n) {
         int flag = 0; //set initial flag = 0
         for (int j = n - 1; j > i; j--)
             if (a[j - 1] > a[j]) {
-                int temp = a[j - 1];
+                int tp = a[j - 1];
                 a[j - 1] = a[j];
-                a[j] = temp;
+                a[j] = tp;
                 flag = 1; //set flag = 1 while swapping
             }
         if (flag == 0) //flag = 0 for no swapping happened
@@ -16,22 +16,40 @@ void bubble_sort(int a[], int n) {
    }
 }
 
-/*Quick Sort 快速排序
+/*Quick Sort 快速排序*/
 void quick_sort(int a[], int low, int high) {
-
+    if (low > high) 
+        return;
+    int i = low;
+    int j = high;
+    int temp = a[low];
+    while (i < j) {
+        while (i < j && a[j] >= temp)
+            j--;
+        while (i < j && a[i] <= temp)
+            i++;
+        if (i < j) {
+            int tp = a[i];
+            a[i] = a[j];
+            a[j] = tp;
+        }
+    }
+    a[low] = a[i];
+    a[i] = temp;
+    quick_sort(a, low, i - 1);
+    quick_sort(a, i + 1, high);
 }
-
-int Partition(int a[], int low, int high) {
-
-
-}
-*/
 
 int main() {
     int a[10];
     for (int i = 0; i < 10; i++)
         scanf("%d", &a[i]);
     bubble_sort(a, 10);
+    printf("Bubble Sort: \n");
+    for (int i = 0; i < 10; i++)
+        printf("%d\n", a[i]);
+    quick_sort(a, 0, 9);
+    printf("Quick Sort: \n");
     for (int i = 0; i < 10; i++)
         printf("%d\n", a[i]);
     return 0;
