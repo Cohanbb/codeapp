9a3d67981e0c7660b5c52ea30c3ce127d5a878b3
Cohanbb <cohanbb777@163.com>
2022-06-12 16:44:18 +0800

    update SeqStack

--- c-cpp/DS/SeqStack.cpp
+++ c-cpp/DS/SeqStack.cpp
@@ -1,5 +1,7 @@
 /*使用数组实现的顺序栈*/
 #include <stdio.h>
+#include <malloc.h>
+
 #define Initsize 20;
 
 typedef struct SeqStack {
@@ -8,27 +10,53 @@ typedef struct SeqStack {
 } sqstack;
 
 void InitStack(sqstack &S) {
-
+    S.maxsize = Initsize;
+    S.data = (int *)malloc(sizeof(int) * S.maxsize);
+    S.top = -1;
 }
 
-void Empty(sqstack S) {
+bool Empty(sqstack S) {
+    if (S.top == -1)
+        return true;
+    return false;
+}
 
+void ExtendStack(sqstack &S, int n) {
+    int *p = S.data;
+    S.maxsize += n;
+    S.data = (int *)malloc(sizeof(int) * S.maxsize);
+    for (int i = 0; i <= S.top; i++)
+        S.data[i] = p[i];
 }
 
 void Push(sqstack &S, int e) {
-
+    if (S.top + 1 == S.maxsize)
+        ExtendStack(S, 1);
+    S.data[++S.top] = e;
 }
 
 void Pop(sqstack &S, int &e) {
-
+    if (Empty(S)) {
+        printf("Empty Stack!");
+        return;
+    }
+    e = S.data[S.top--];
 }
 
 void PrintStack(sqstack S) {
-
+    if (Empty(S)) {
+        printf("Empty Stack!");
+        return;
+    }
+    for (int i = 0; i <= S.top; i++)
+        printf("%d\n", S.data[i]);
 }
 
 void Destroy(sqstack *S) {
-
+    free(S->data);
+    S->data = NULL;
+    free(S);
+    S = NULL;
 }
 
 int main() {
