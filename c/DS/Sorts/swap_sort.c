24dc545ee0588cbeec961176be29dc86c08349fc
Cohanbb <76557867+Cohanbb@users.noreply.github.com>
2022-07-25 22:15:38 +0800

    Update swap_sort.c

--- c/DS/Sorts/swap_sort.c
+++ c/DS/Sorts/swap_sort.c
@@ -2,9 +2,9 @@
 
 /* Bubble Sort 冒泡排序 */
 void bubble_sort(int a[], int n) {
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; ++i) {
         int flag = 0; //set initial flag = 0
-        for (int j = n - 1; j > i; j--)
+        for (int j = n - 1; j > i; --j)
             if (a[j - 1] > a[j]) {
                 int tp = a[j - 1];
                 a[j - 1] = a[j];
@@ -25,9 +25,9 @@ void quick_sort(int a[], int low, int high) {
     int temp = a[low];
     while (i < j) {
         while (i < j && a[j] >= temp)
-            j--;
+            --j;
         while (i < j && a[i] <= temp)
-            i++;
+            ++i;
         if (i < j) {
             int tp = a[i];
             a[i] = a[j];
@@ -41,13 +41,13 @@ void quick_sort(int a[], int low, int high) {
 }
 
 void showarray(int a[], int n) {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i)
         printf("%d\n", a[i]);
 }
 
 int main() {
     int a[10];
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; ++i)
         scanf("%d", &a[i]);
 
     bubble_sort(a, 10);
