a31a01d6b7fb9f774c7d2b2f81b984c617d364b4
Cohanbb <76557867+Cohanbb@users.noreply.github.com>
2022-07-25 22:06:56 +0800

    Update SeqList_dynamic.c

--- c/DS/SeqList_dynamic/SeqList_dynamic.c
+++ c/DS/SeqList_dynamic/SeqList_dynamic.c
@@ -4,7 +4,7 @@
 /* Initialize Sequential List 初始化顺序表 */
 void InitList(sqlistd *L) {
     L->maxsize = Initsize;
-    L->data = (int *)malloc(sizeof(int) * L->maxsize);
+    L->data = (int *)malloc(sizeof (int) * L->maxsize);
     L->length = 0;
     for (int i = 0; i < L->maxsize; ++i)
         *(L->data + i) = 0;
@@ -18,7 +18,7 @@ int Empty(sqlistd L) {
 void ExtendList(sqlistd *L, int n) {
     int *p = L->data; // p 指向原地址
     L->maxsize += n; // 扩容
-    L->data = (int *)malloc(sizeof(int) * L->maxsize); // 开辟新地址
+    L->data = (int *)malloc(sizeof (int) * L->maxsize); // 开辟新地址
     for (int i = 0; i < L->length; ++i)
         *(L->data + i) = *(p + i);
     free(p); // 释放 p 指向的内存
@@ -36,7 +36,7 @@ void InsertElem(sqlistd *L, int i, int e) {
     for (int j = L->length; j >= i; --j)
         *(L->data + j) = *(L->data + j - 1);
     *(L->data + i - 1) = e;
-    ++(L->length);
+    ++L->length;
 }
 
 /* Delete Element 删除元素 */
@@ -49,7 +49,7 @@ void DeleteElem(sqlistd *L, int i, int *e) {
     for (int j = i; j < L->length; ++j)
         *(L->data + j - 1) = *(L->data + j);
     *(L->data + L->length - 1) = 0;
-    --(L->length);
+    --L->length;
 }
 
 /* Search Element By Value 按值查找 */
